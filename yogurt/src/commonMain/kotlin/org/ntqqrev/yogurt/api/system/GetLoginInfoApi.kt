5944886c88313c7ff7ffa72e6fdfe3261239fa7c
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-14 00:46:21 +0800

    feat(yogurt): use generated, completed Milky type declaration

--- yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/api/system/GetLoginInfoApi.kt
+++ yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/api/system/GetLoginInfoApi.kt
@@ -1,11 +1,11 @@
 package org.ntqqrev.yogurt.api.system
 
 import org.ntqqrev.yogurt.api.routeMilkyApi
-import org.ntqqrev.yogurt.protocol.ApiEmptyStruct
-import org.ntqqrev.yogurt.protocol.GetLoginInfoResponse
+import org.ntqqrev.yogurt.protocol.GetLoginInfoInput
+import org.ntqqrev.yogurt.protocol.GetLoginInfoOutput
 
-val routeGetLoginInfoApi = routeMilkyApi<ApiEmptyStruct, GetLoginInfoResponse>("get_login_info") {
-    GetLoginInfoResponse(
+val routeGetLoginInfoApi = routeMilkyApi<GetLoginInfoInput, GetLoginInfoOutput>("get_login_info") {
+    GetLoginInfoOutput(
         uin = uin,
         nickname = "TODO" // todo: resolve nickname
     )

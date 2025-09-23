f889d37bf586e111e42b062f8df0a36205a50d01
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-23 17:46:46 +0800

    update(core): optimize sign cmd list

--- acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/internal/logic/PacketLogic.kt
+++ acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/internal/logic/PacketLogic.kt
@@ -36,9 +36,6 @@ internal class PacketLogic(client: LagrangeClient) : AbstractLogic(client) {
     private val headerLength = 4
     var connected = false
     val signRequiredCommand = setOf(
-        "trpc.o3.ecdh_access.EcdhAccess.SsoEstablishShareKey",
-        "trpc.o3.ecdh_access.EcdhAccess.SsoSecureAccess",
-        "trpc.o3.report.Report.SsoReport",
         "MessageSvc.PbSendMsg",
         "wtlogin.trans_emp",
         "wtlogin.login",
@@ -48,31 +45,6 @@ internal class PacketLogic(client: LagrangeClient) : AbstractLogic(client) {
         "trpc.login.ecdh.EcdhService.SsoNTLoginPasswordLoginNewDevice",
         "trpc.login.ecdh.EcdhService.SsoNTLoginEasyLoginUnusualDevice",
         "trpc.login.ecdh.EcdhService.SsoNTLoginPasswordLoginUnusualDevice",
-        "OidbSvcTrpcTcp.0x11ec_1",
-        "OidbSvcTrpcTcp.0x758_1", // create a group
-        "OidbSvcTrpcTcp.0x7c1_1",
-        "OidbSvcTrpcTcp.0x7c2_5", // request friend
-        "OidbSvcTrpcTcp.0x10db_1",
-        "OidbSvcTrpcTcp.0x8a1_7", // request group
-        "OidbSvcTrpcTcp.0x89a_0",
-        "OidbSvcTrpcTcp.0x89a_15",
-        "OidbSvcTrpcTcp.0x88d_0", // fetch group detail
-        "OidbSvcTrpcTcp.0x88d_14",
-        "OidbSvcTrpcTcp.0x112a_1",
-        "OidbSvcTrpcTcp.0x587_74",
-        "OidbSvcTrpcTcp.0x1100_1",
-        "OidbSvcTrpcTcp.0x1102_1",
-        "OidbSvcTrpcTcp.0x1103_1",
-        "OidbSvcTrpcTcp.0x1107_1",
-        "OidbSvcTrpcTcp.0x1105_1",
-        "OidbSvcTrpcTcp.0xf88_1",
-        "OidbSvcTrpcTcp.0xf89_1",
-        "OidbSvcTrpcTcp.0xf57_1",
-        "OidbSvcTrpcTcp.0xf57_106",
-        "OidbSvcTrpcTcp.0xf57_9",
-        "OidbSvcTrpcTcp.0xf55_1",
-        "OidbSvcTrpcTcp.0xf67_1",
-        "OidbSvcTrpcTcp.0xf67_5",
         "OidbSvcTrpcTcp.0x6d9_4"
     )
 

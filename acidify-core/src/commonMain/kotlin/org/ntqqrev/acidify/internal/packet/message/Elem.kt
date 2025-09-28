98897121696339ddf50c4068013231247eebbbcf
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-28 20:48:54 +0800

    fix(core): add necessary `optional`

--- acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/internal/packet/message/Elem.kt
+++ acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/internal/packet/message/Elem.kt
@@ -1,17 +1,18 @@
 package org.ntqqrev.acidify.internal.packet.message
 
 import org.ntqqrev.acidify.internal.packet.message.elem.*
+import org.ntqqrev.acidify.pb.PbOptional
 import org.ntqqrev.acidify.pb.PbSchema
 import org.ntqqrev.acidify.pb.get
 
 internal object Elem : PbSchema() {
-    val text = Text[1]
-    val notOnlineImage = NotOnlineImage[4]
-    val transElemInfo = TransElem[5]
-    val customFace = CustomFace[8]
-    val richMsg = RichMsg[12]
-    val videoFile = VideoFile[19]
-    val srcMsg = SourceMsg[45]
-    val lightAppElem = LightAppElem[51]
-    val commonElem = CommonElem[53]
+    val text = PbOptional[Text[1]]
+    val notOnlineImage = PbOptional[NotOnlineImage[4]]
+    val transElemInfo = PbOptional[TransElem[5]]
+    val customFace = PbOptional[CustomFace[8]]
+    val richMsg = PbOptional[RichMsg[12]]
+    val videoFile = PbOptional[VideoFile[19]]
+    val srcMsg = PbOptional[SourceMsg[45]]
+    val lightAppElem = PbOptional[LightAppElem[51]]
+    val commonElem = PbOptional[CommonElem[53]]
 }

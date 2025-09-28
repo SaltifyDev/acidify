19d575fcf7438ad33473229e0bbde0e4f6ec6a5b
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-29 01:19:48 +0800

    refactor(yogurt): rewrite launching logic, putting most fields inside the method

--- yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/YogurtApp.kt
+++ yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/YogurtApp.kt
@@ -30,6 +30,7 @@ import org.ntqqrev.acidify.common.SessionStore
 import org.ntqqrev.acidify.event.QRCodeGeneratedEvent
 import org.ntqqrev.acidify.event.SessionStoreUpdatedEvent
 import org.ntqqrev.acidify.util.UrlSignProvider
+import org.ntqqrev.acidify.util.log.Logger
 import org.ntqqrev.milky.ApiGeneralResponse
 import org.ntqqrev.milky.milkyJsonModule
 import org.ntqqrev.yogurt.api.configureMilkyApi
@@ -40,23 +41,26 @@ import org.ntqqrev.yogurt.util.logHandler
 
 object YogurtApp {
     val config = YogurtConfig.loadFromFile()
-    val signProvider = UrlSignProvider(config.signApiUrl)
-    val sessionStorePath = Path("session-store.json")
-    val sessionStore: SessionStore = if (SystemFileSystem.exists(sessionStorePath)) {
-        SystemFileSystem.source(sessionStorePath).buffered().use {
-            Json.decodeFromSource<SessionStore>(it)
-        }
-    } else {
-        val emptySessionStore = SessionStore.empty()
-        SystemFileSystem.sink(sessionStorePath).buffered().use {
-            Json.encodeToSink(emptySessionStore, it)
-        }
-        emptySessionStore
-    }
     val scope = CoroutineScope(Dispatchers.IO)
-    val qrCodePath = Path("qrcode.png")
 
-    fun start() = runBlocking {
+    fun createServer() = embeddedServer(
+        factory = CIO,
+        port = config.httpConfig.port,
+        host = config.httpConfig.host
+    ) {
+        val signProvider = UrlSignProvider(config.signApiUrl)
+        val sessionStorePath = Path("session-store.json")
+        val sessionStore: SessionStore = if (SystemFileSystem.exists(sessionStorePath)) {
+            SystemFileSystem.source(sessionStorePath).buffered().use {
+                Json.decodeFromSource<SessionStore>(it)
+            }
+        } else {
+            val emptySessionStore = SessionStore.empty()
+            SystemFileSystem.sink(sessionStorePath).buffered().use {
+                Json.encodeToSink(emptySessionStore, it)
+            }
+            emptySessionStore
+        }
         val appInfo = signProvider.getAppInfo()!!
         val bot = Bot.create(
             appInfo = appInfo,
@@ -77,128 +81,130 @@ object YogurtApp {
 
         val logger = bot.createLogger(this@YogurtApp)
 
-        embeddedServer(
-            factory = CIO,
-            port = config.httpConfig.port,
-            host = config.httpConfig.host
-        ) {
-            install(ContentNegotiation) {
-                json(milkyJsonModule)
+        install(ContentNegotiation) {
+            json(milkyJsonModule)
+        }
+        install(WebSockets) {
+            contentConverter = KotlinxWebsocketSerializationConverter(milkyJsonModule)
+        }
+        install(SSE)
+
+        dependencies {
+            provide { bot } cleanup {
+                runBlocking { bot.offline() }
             }
+            provide { logger }
+        }
+        configureCacheDeps()
 
-            install(WebSockets) {
-                contentConverter = KotlinxWebsocketSerializationConverter(milkyJsonModule)
-            }
-
-            install(SSE)
-
-            dependencies {
-                provide { bot } cleanup {
-                    runBlocking { bot.offline() }
-                }
-                provide { logger }
-            }
-
-            configureCacheDeps()
-
-            routing {
-                route("/api") {
-                    if (config.httpConfig.accessToken.isNotEmpty()) {
-                        val auth = createRouteScopedPlugin("Auth") {
-                            onCall { call ->
-                                if (call.request.headers["Authorization"] != "Bearer ${config.httpConfig.accessToken}") {
-                                    call.respond(HttpStatusCode.Unauthorized)
-                                    return@onCall
-                                }
-                            }
-                        }
-                        install(auth)
-                    }
-
-                    val protectNotLoggedIn = createRouteScopedPlugin("ProtectNotLoggedIn") {
+        routing {
+            route("/api") {
+                if (config.httpConfig.accessToken.isNotEmpty()) {
+                    val auth = createRouteScopedPlugin("Auth") {
                         onCall { call ->
-                            if (!bot.isLoggedIn) {
-                                call.respond(
-                                    ApiGeneralResponse(
-                                        status = "failed",
-                                        retcode = -403,
-                                        message = "Bot is not logged in"
-                                    )
-                                )
+                            if (call.request.headers["Authorization"] != "Bearer ${config.httpConfig.accessToken}") {
+                                call.respond(HttpStatusCode.Unauthorized)
                                 return@onCall
                             }
                         }
                     }
-                    install(protectNotLoggedIn)
-
-                    configureMilkyApi()
+                    install(auth)
                 }
 
-                route("/event") {
-                    if (config.httpConfig.accessToken.isNotEmpty()) {
-                        val auth = createRouteScopedPlugin("Auth") {
-                            onCall { call ->
-                                if (
-                                    call.request.headers["Authorization"] != "Bearer ${config.httpConfig.accessToken}" &&
-                                    call.request.queryParameters["access_token"] != config.httpConfig.accessToken
-                                ) {
-                                    call.respond(HttpStatusCode.Unauthorized)
-                                    return@onCall
-                                }
-                            }
-                        }
-                        install(auth)
-                    }
-
-                    webSocket {
-                        logger.i { "${call.request.local.remoteAddress} 通过 WebSocket 连接" }
-                        launch {
-                            bot.eventFlow.collect { event ->
-                                transformAcidifyEvent(event)?.let { sendSerialized(it) }
-                            }
-                        }
-                        try {
-                            incoming.receive()
-                        } catch (_: ClosedReceiveChannelException) {
-                            logger.i { "${call.request.local.remoteAddress} 断开 WebSocket 连接" }
+                val protectNotLoggedIn = createRouteScopedPlugin("ProtectNotLoggedIn") {
+                    onCall { call ->
+                        if (!bot.isLoggedIn) {
+                            call.respond(
+                                ApiGeneralResponse(
+                                    status = "failed",
+                                    retcode = -403,
+                                    message = "Bot is not logged in"
+                                )
+                            )
+                            return@onCall
                         }
                     }
+                }
+                install(protectNotLoggedIn)
 
-                    sse {
-                        logger.i { "${call.request.local.remoteAddress} 通过 SSE 连接" }
-                        launch {
-                            bot.eventFlow.collect { event ->
-                                transformAcidifyEvent(event)?.let {
-                                    send(
-                                        data = milkyJsonModule.encodeToString(it),
-                                        event = "milky_event"
-                                    )
-                                }
+                configureMilkyApi()
+            }
+
+            route("/event") {
+                if (config.httpConfig.accessToken.isNotEmpty()) {
+                    val auth = createRouteScopedPlugin("Auth") {
+                        onCall { call ->
+                            if (
+                                call.request.headers["Authorization"] != "Bearer ${config.httpConfig.accessToken}" &&
+                                call.request.queryParameters["access_token"] != config.httpConfig.accessToken
+                            ) {
+                                call.respond(HttpStatusCode.Unauthorized)
+                                return@onCall
+                            }
+                        }
+                    }
+                    install(auth)
+                }
+
+                webSocket {
+                    logger.i { "${call.request.local.remoteAddress} 通过 WebSocket 连接" }
+                    launch {
+                        bot.eventFlow.collect { event ->
+                            transformAcidifyEvent(event)?.let { sendSerialized(it) }
+                        }
+                    }
+                    try {
+                        incoming.receive()
+                    } catch (_: ClosedReceiveChannelException) {
+                        logger.i { "${call.request.local.remoteAddress} 断开 WebSocket 连接" }
+                    }
+                }
+
+                sse {
+                    logger.i { "${call.request.local.remoteAddress} 通过 SSE 连接" }
+                    launch {
+                        bot.eventFlow.collect { event ->
+                            transformAcidifyEvent(event)?.let {
+                                send(
+                                    data = milkyJsonModule.encodeToString(it),
+                                    event = "milky_event"
+                                )
                             }
                         }
                     }
                 }
             }
-        }.start()
-
-        scope.launch {
-            bot.eventFlow.filterIsInstance<QRCodeGeneratedEvent>()
-                .take(1)
-                .collect {
-                    logger.i { "请用手机 QQ 扫描二维码：\n" + generateTerminalQRCode(it.url) }
-                    logger.i { "或使用以下 URL 生成二维码并扫描：" }
-                    logger.i { it.url }
-                    SystemFileSystem.sink(qrCodePath).buffered().use { sink ->
-                        sink.write(it.png)
-                    }
-                    logger.i { "二维码文件已保存至 ${SystemFileSystem.resolve(qrCodePath)}" }
-                }
         }
+    }
 
-        if (sessionStore.a2.isEmpty()) {
-            bot.qrCodeLogin()
-        } else {
-            bot.tryLogin()
+    fun start() = runBlocking {
+        val embeddedServer = createServer()
+        embeddedServer.start(wait = false)
+
+        with(embeddedServer.application) {
+            val bot = dependencies.resolve<Bot>()
+            val logger = dependencies.resolve<Logger>()
+            val qrCodePath = Path("qrcode.png")
+
+            scope.launch {
+                bot.eventFlow.filterIsInstance<QRCodeGeneratedEvent>()
+                    .take(1)
+                    .collect {
+                        logger.i { "请用手机 QQ 扫描二维码：\n" + generateTerminalQRCode(it.url) }
+                        logger.i { "或使用以下 URL 生成二维码并扫描：" }
+                        logger.i { it.url }
+                        SystemFileSystem.sink(qrCodePath).buffered().use { sink ->
+                            sink.write(it.png)
+                        }
+                        logger.i { "二维码文件已保存至 ${SystemFileSystem.resolve(qrCodePath)}" }
+                    }
+            }
+
+            if (bot.sessionStore.a2.isEmpty()) {
+                bot.qrCodeLogin()
+            } else {
+                bot.tryLogin()
+            }
         }
     }
 }

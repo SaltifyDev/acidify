a9231a7d6dc64268efa6694cd514369a0e23ed5e
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-29 10:07:02 +0800

    feat(yogurt): apply `reportSelfMessage` for event reporting

--- yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/transform/EventTransform.kt
+++ yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/transform/EventTransform.kt
@@ -1,15 +1,25 @@
 package org.ntqqrev.yogurt.transform
 
-import io.ktor.server.application.Application
+import io.ktor.server.application.*
+import io.ktor.server.plugins.di.*
+import org.ntqqrev.acidify.Bot
 import org.ntqqrev.acidify.event.AcidifyEvent
 import org.ntqqrev.acidify.event.MessageReceiveEvent
 import org.ntqqrev.milky.Event
+import org.ntqqrev.yogurt.YogurtApp.config
 
 suspend fun Application.transformAcidifyEvent(event: AcidifyEvent): Event? {
+    val bot = dependencies.resolve<Bot>()
     return when (event) {
-        is MessageReceiveEvent -> Event.MessageReceive(
-            data = transformMessage(event.message) ?: return null
-        )
+        is MessageReceiveEvent -> {
+            if (config.reportSelfMessage || event.message.senderUin != bot.uin) {
+                Event.MessageReceive(
+                    data = transformMessage(event.message) ?: return null
+                )
+            } else {
+                null
+            }
+        }
 
         else -> null
     }

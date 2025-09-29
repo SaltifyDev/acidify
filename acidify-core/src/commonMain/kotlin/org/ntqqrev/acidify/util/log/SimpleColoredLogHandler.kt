b55bc81cec563c59246e6afa96d49752a64a98e0
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-29 09:54:14 +0800

    refactor(core): expose `shortenPackageName`

--- acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/util/log/SimpleColoredLogHandler.kt
+++ acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/util/log/SimpleColoredLogHandler.kt
@@ -11,18 +11,6 @@ import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
 object SimpleColoredLogHandler : LogHandler {
-    private fun shortenPackageName(tag: String): String {
-        val parts = tag.split('.')
-        val b = StringBuilder()
-        for (i in 0 until parts.size - 1) {
-            b.append(parts[i][0])
-            b.append('.')
-        }
-        b.append(parts.last())
-        b.padEnd(30)
-        return b.toString()
-    }
-
     private val timeFormat = LocalDateTime.Format {
         hour()
         char(':')

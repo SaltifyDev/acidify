41ebae138231da43fd5b0a0cec3952f42c290b0c
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-29 09:54:53 +0800

    update(yogurt): optimize jvm logging line length

--- yogurt/src/jvmMain/kotlin/org/ntqqrev/yogurt/util/LoggingUtils.jvm.kt
+++ yogurt/src/jvmMain/kotlin/org/ntqqrev/yogurt/util/LoggingUtils.jvm.kt
@@ -3,10 +3,11 @@ package org.ntqqrev.yogurt.util
 import io.ktor.util.logging.*
 import org.ntqqrev.acidify.util.log.LogHandler
 import org.ntqqrev.acidify.util.log.LogLevel
+import org.ntqqrev.acidify.util.log.shortenPackageName
 
 private fun Logger.toAcidifyLogHandler(): LogHandler =
     LogHandler { level, tag, message, throwable ->
-        val trueMessage = "[$tag] $message"
+        val trueMessage = "[${shortenPackageName(tag)}] $message"
         when (level) {
             LogLevel.VERBOSE -> trace(trueMessage)
             LogLevel.DEBUG -> debug(trueMessage)

b3d9305ea80b2e74537ad3a595604bff7eacd82d
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-30 21:54:46 +0800

    feat(codec/test): add test on audio to pcm

--- yogurt-media-codec/src/commonTest/kotlin/CodecTest.kt
+++ yogurt-media-codec/src/commonTest/kotlin/CodecTest.kt
@@ -2,11 +2,18 @@ import kotlinx.io.buffered
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 import kotlinx.io.readByteArray
+import org.ntqqrev.yogurt.codec.audioToPcm24000
 import org.ntqqrev.yogurt.codec.getImageInfo
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class CodecTest {
+    val testOutputPath = Path("test-output").also {
+        if (!SystemFileSystem.exists(it)) {
+            SystemFileSystem.createDirectories(it)
+        }
+    }
+
     @Test
     fun imageDecodeTest() {
         listOf(
@@ -23,6 +30,18 @@ class CodecTest {
             val info = getImageInfo(file)
             assertEquals(info.format.ext, it)
             println("Image info of test.$it: $info")
+        }
+    }
+
+    @Test
+    fun audioToPcmTest() {
+        val file = SystemFileSystem.source(Path("src/commonTest/resources/audio/test.mp3"))
+            .buffered()
+            .readByteArray()
+        val pcm = audioToPcm24000(file)
+        println("PCM size: ${pcm.size}")
+        SystemFileSystem.sink(Path("test-output/test-pcm-24000.pcm")).buffered().use {
+            it.write(pcm)
         }
     }
 }

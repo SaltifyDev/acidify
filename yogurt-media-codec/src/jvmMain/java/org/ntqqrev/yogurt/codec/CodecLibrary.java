4f4a968f1baa258ca127608c316f4c95f2db2a4d
Wesley F. Young <wesley.f.young@outlook.com>
2025-10-01 10:37:26 +0800

    fix(codec): fix module loading under JVM+POSIX

--- yogurt-media-codec/src/jvmMain/java/org/ntqqrev/yogurt/codec/CodecLibrary.java
+++ yogurt-media-codec/src/jvmMain/java/org/ntqqrev/yogurt/codec/CodecLibrary.java
@@ -6,34 +6,39 @@ import com.sun.jna.Pointer;
 import com.sun.jna.ptr.PointerByReference;
 import com.sun.jna.ptr.IntByReference;
 
+import java.nio.file.Path;
+
 public interface CodecLibrary extends Library {
     static String getLibraryPath() {
-        String libraryName = "lagrangecodec";
         String osName = System.getProperty("os.name").toLowerCase();
         String arch = System.getProperty("os.arch").toLowerCase();
         if (osName.contains("win")) {
             if (arch.contains("64")) {
-                return "./lib/windows-x64/" + libraryName;
+                return "./lib/windows-x64/lagrangecodec";
             } else {
-                return "./lib/windows-x86/" + libraryName;
+                return "./lib/windows-x86/lagrangecodec";
             }
         } else if (osName.contains("mac")) {
             if (arch.contains("aarch64") || arch.contains("arm64")) {
-                return "./lib/macos-arm64/" + libraryName;
+                return "./lib/macos-arm64/liblagrangecodec.dylib";
             } else {
-                return "./lib/macos-x64/" + libraryName;
+                return "./lib/macos-x64/liblagrangecodec.dylib";
             }
         } else if (osName.contains("nux") || osName.contains("nix")) {
             if (arch.contains("aarch64") || arch.contains("arm64")) {
-                return "./lib/linux-arm64/" + libraryName;
+                return "./lib/linux-arm64/liblagrangecodec.so";
             } else if (arch.contains("x86_64") || arch.contains("amd64")) {
-                return "./lib/linux-x64/" + libraryName;
+                return "./lib/linux-x64/liblagrangecodec.so";
             }
         }
         throw new UnsupportedOperationException("Unsupported OS or architecture: " + osName + " " + arch);
     }
 
-    CodecLibrary INSTANCE = Native.load(getLibraryPath(), CodecLibrary.class);
+    static String getLibraryAbsolutePath() {
+        return Path.of(getLibraryPath()).toAbsolutePath().toString();
+    }
+
+    CodecLibrary INSTANCE = Native.load(getLibraryAbsolutePath(), CodecLibrary.class);
 
     int audio_to_pcm(Pointer data, int length, AudioCodecCallback callback, Pointer userData);
 

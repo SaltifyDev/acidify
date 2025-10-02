7239643af51a68c555293e1a48289c8c2e063d62
Wesley F. Young <wesley.f.young@outlook.com>
2025-10-02 14:58:11 +0800

    fix: set sslVerify based on platform version

--- acidify-core/src/nativeMain/kotlin/org/ntqqrev/acidify/util/HttpClientFactory.native.kt
+++ acidify-core/src/nativeMain/kotlin/org/ntqqrev/acidify/util/HttpClientFactory.native.kt
@@ -2,11 +2,16 @@ package org.ntqqrev.acidify.util
 
 import io.ktor.client.*
 import io.ktor.client.engine.curl.*
+import kotlin.experimental.ExperimentalNativeApi
 
+@OptIn(ExperimentalNativeApi::class)
 actual fun createHttpClient(block: HttpClientConfig<*>.() -> Unit): HttpClient {
     return HttpClient(Curl) {
         engine {
-            sslVerify = false
+            sslVerify = when (Platform.osFamily) {
+                OsFamily.WINDOWS -> false
+                else -> true
+            }
         }
         block()
     }
